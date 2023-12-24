ef79be4adbe3f1789f9cb8127c76d9c062cb3ac5
unfear-underscore <107675637+unfear-underscore@users.noreply.github.com>
2023-12-24 14:53:21 +0000

    Added recipe for biome pointer

--- src/main/java/me/unfear/BiomePointer/ConfigLoader.java
+++ src/main/java/me/unfear/BiomePointer/ConfigLoader.java
@@ -4,19 +4,25 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.block.Biome;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.inventory.ShapedRecipe;
 
 public class ConfigLoader {
 
 	private int searchDistance, cacheDistance;
 	private long updateTime;
 	private HashSet<Biome> biomes;
+	private final FileConfiguration config;
 
 	public ConfigLoader() {
 		BiomePointer.inst.saveDefaultConfig();
 
-		final FileConfiguration config = BiomePointer.inst.getConfig();
+		config = BiomePointer.inst.getConfig();
 		this.biomes = new HashSet<>();
 
 		this.searchDistance = config.getInt("search-distance", 2000);
@@ -60,6 +66,56 @@ public class ConfigLoader {
 			BiomePointer.inst.getServer().getPluginManager().disablePlugin(BiomePointer.inst);
 			return;
 		}
+
+		registerRecipe();
+	}
+
+	private void registerRecipe() {
+		if (!config.getBoolean("recipe.enabled", false)) return; // Recipe disabled
+
+		NamespacedKey key = new NamespacedKey(BiomePointer.inst, "biomepointer");
+		ShapedRecipe recipe = new ShapedRecipe(key, BiomePointer.inst.createBiomePointer());
+		List<String> shapeList = config.getStringList("recipe.shape");
+		if (shapeList.size() != 3) {
+			BiomePointer.inst.getLogger().severe("Failed to register recipe, shape didn't have 3 rows");
+			return;
+		}
+		for (String row : shapeList) {
+			if (row.length() != 3) {
+				BiomePointer.inst.getLogger().severe("Failed to register recipe, shape didn't have 3 columns");
+				return;
+			}
+		}
+
+		String[] shape = new String[3];
+		shapeList.toArray(shape);
+		recipe.shape(shape);
+
+		ConfigurationSection ingredientsSection = config.getConfigurationSection("recipe.ingredients");
+		if (ingredientsSection == null) {
+			BiomePointer.inst.getLogger().severe("Failed to register recipe, ingredients config section doesn't exist");
+			return;
+		}
+
+		for (String iKey : ingredientsSection.getKeys(false)) {
+			if (iKey.length() != 1) {
+				BiomePointer.inst.getLogger().severe("Failed to register recipe, ingredient letter '" + iKey + "' wasn't 1 letter long");
+				return;
+			}
+
+			Material m = null;
+			try {
+				m = Material.valueOf(ingredientsSection.getString(iKey));
+			} catch (IllegalArgumentException ignored) {}
+			if (m == null) {
+				BiomePointer.inst.getLogger().severe("Failed to register recipe, " + m + " isn't a valid material");
+				return;
+			}
+
+			recipe.setIngredient(iKey.charAt(0), m);
+		}
+
+		Bukkit.addRecipe(recipe);
 	}
 
 	public int getSearchDistance() {
